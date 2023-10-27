43699e3463d3f66ea8f11ec5f51e8b6635d56103
joon shim <jshim4996@gmail.com>
2023-10-27 18:27:39 +0900

    기본 로그인 페이지 상단 ,하단 구성

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/screens/sign_up/sign_up_screen.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -13,7 +16,7 @@ class TikTokApp extends StatelessWidget {
     return MaterialApp(
       title: 'TikTokClone',
       theme: ThemeData(primaryColor: const Color(0xFFE9435A)),
-      home: Container(),
+      home: const SignUpScreen(),
     );
   }
 }
